4eaf1a854c056d9eb9132a9e970fb9aed5be989e
Rodney Edward <elitereward@gmail.com>
2023-11-03 16:23:48 -0400

    deleted

--- Beginner/Project_1_Band_Name_Generator_20230906_v1.0.py
+++
@@ -1,9 +0,0 @@
-# A simple band name generator that generates a band name based on user input
-# Print a welcome message
-print("Welcome to the Band Name Generator!")
-# Ask user for the city they grew up in
-city_name = input("What is the name of the city you grew up in?\n")
-# Ask user for the name of their pet
-pet_name = input("What is the name of your pet?\n")
-# Combine city name and pet name to show band name
-print("Your band name could be " + city_name + " " + pet_name + "!")
